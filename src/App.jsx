9743ac0ec77ed5e9b38733744b487891b0f68224
rafiul1000 <rafiulislam5075@gmail.com>
2023-09-12 12:44:40 +0600

    Handle bookmark state and display bookmark

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,18 @@ import Bookmarks from "./components/Bookmarks/Bookmarks";
 import Header from "./components/Header/Header";
 
 function App() {
-  const [bookmark, setBookmarks] = useState([]);
+  const [bookmarks, setBookmarks] = useState([]);
 
   const handleAddToBookmark = blog => {
-    console.log('bookmark adding soon')
+    const newBookmarks = [...bookmarks, blog];
+    setBookmarks(newBookmarks);
   }
   return (
     <>
       <Header></Header>
       <div className="md:flex max-w-7xl mx-auto">
         <Blogs handleAddToBookmark={handleAddToBookmark}></Blogs>
-        <Bookmarks></Bookmarks>
+        <Bookmarks bookmarks={bookmarks}></Bookmarks>
       </div>
     </>
   );
