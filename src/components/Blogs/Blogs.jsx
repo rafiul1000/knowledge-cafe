01429928bed3438e8d646665e0f7d6ff9d4b075e
rafiul1000 <rafiulislam5075@gmail.com>
2023-09-12 13:41:36 +0600

    Add Time when marked as read

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Blog from "../Blog/Blog";
 import PropTypes from 'prop-types';
 
-const Blogs = ({handleAddToBookmark}) => {
+const Blogs = ({handleAddToBookmark, handleMarkAsRead}) => {
   const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
@@ -17,6 +17,7 @@ const Blogs = ({handleAddToBookmark}) => {
       <h1 className="text-4xl">Blogs: {blogs.length}</h1>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} handleAddToBookmark={handleAddToBookmark}
+        handleMarkAsRead={handleMarkAsRead}
         ></Blog>
       ))}
     </div>
@@ -24,7 +25,8 @@ const Blogs = ({handleAddToBookmark}) => {
 };
 
 Blogs.propTypes = {
-  handleAddToBookmark: PropTypes.func
+  handleAddToBookmark: PropTypes.func,
+  handleMarkAsRead: PropTypes.func
 }
 
 export default Blogs;
